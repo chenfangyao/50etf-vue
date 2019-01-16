97a7418c7be56ed94f089d5e59e59118b367f40e
qiuhuasheng <826862051@qq.com>
2019-01-16 14:50:55 +0800

    111

--- pages/mine_sub/invite_friends/invite_friends.vue
+++ pages/mine_sub/invite_friends/invite_friends.vue
@@ -26,7 +26,7 @@ export default {
        codeValue:'http://50etfvue.com:1688/h5#/pages/register/register',
        // imageUrl:'http://img.zcool.cn/community/01f9ea56e282836ac72531cbe0233b.jpg@2o.jpg',//默认二维码中间图片
        // imageUrl:require('../../../static/loginResgImg/logo.png'),//默认二维码中间图片
-       imageUrl:'h5/static/loginResgImg/logo.png',//默认二维码中间图片
+       imageUrl:'/h5/static/loginResgImg/logo.png',//默认二维码中间图片
     };
   },
 	computed: {
