9df7c032fb5bd7f7e97e6de5289fcd352270ac16
abcdefg123 <383569403@qq.com>
2019-02-11 13:34:57 +0800

    实名认证

--- src/pages/mine_sub/credentials_upload/tep3/tep3.vue
+++ src/pages/mine_sub/credentials_upload/tep3/tep3.vue
@@ -1,42 +1,55 @@
 <template>
-<div class="wrap">
-	<div class="wrap-2">
-		<base-header has-back='1'></base-header>
-    <div class="line1">证件信息已提交</div>
-    <div class="line2">信息正在审核</div>
-    <div class="line3">审核时间：</div>
-    <div class="line3">工作日10:00-18:00：将在30分钟左右完成审核18:00后将在次日审核</div>
-	</div>
-  <div class="fixBottom">
-    <btn-block txt='确定' @v-tap='goNext'></btn-block>
+  <div class="wrap">
+    <div class="wrap-2">
+      <base-header has-back="1"></base-header>
+      <div class="line1">证件信息已提交</div>
+      <div class="line2">信息正在审核</div>
+      <div class="line3">审核时间：</div>
+      <div class="line3">工作日10:00-18:00：将在30分钟左右完成审核18:00后将在次日审核</div>
+    </div>
+    <div class="fixBottom">
+      <btn-block txt="确定" @v-tap="goNext"></btn-block>
+    </div>
   </div>
-</div>
-
 </template>
 
 <script>
 import btnBlock from '@/components/btnBlock.vue'
+import { mapMutations } from 'vuex';
 
 export default {
   components: { btnBlock },
-  methods:{
-    goNext(){
-      this.$redirectTo({url:'/mine'})
-    }
+  methods: {
+    ...mapMutations(['setuserinfo']),
+    goNext() {
+      var options = {
+        url: '/Sapi/User/info',
+        method: 'GET',
+      }
+      this.$httpReq(options).then((res) => {
+        if (res.status) {
+          this.setuserinfo(res.data)
+          this.$redirectTo({ url: '/mine' })
+        }
+      }).catch((err) => {
+        console.error(err, '捕捉')
+      })
+    },
+
   }
 }
 </script>
 
 <style lang='scss' scoped>
-div.wrap{
+div.wrap {
   min-height: 100vh;
 }
 div.wrap-2 {
-  padding-left:.40rem;
+  padding-left: 0.4rem;
   .line1 {
     font-size: 18px;
     color: rgba(51, 51, 51, 1);
-    margin:1.06rem 0 .40rem;
+    margin: 1.06rem 0 0.4rem;
     line-height: 18px;
   }
   .line2 {
@@ -44,17 +57,17 @@ div.wrap-2 {
     font-weight: bold;
     color: rgba(51, 51, 51, 1);
     line-height: 30px;
-    margin-bottom:.82rem;
+    margin-bottom: 0.82rem;
   }
   .line3 {
     font-size: 14px;
     color: rgba(136, 136, 136, 1);
-    margin-right:1.29rem;
+    margin-right: 1.29rem;
   }
 }
-div.fixBottom{
+div.fixBottom {
   position: absolute;
-  bottom:.20rem;
+  bottom: 0.2rem;
   left: 0;
   right: 0;
 }
