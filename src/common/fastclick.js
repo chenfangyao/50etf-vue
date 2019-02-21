76b26ae7bb1a2249a71a51014de3305a5dc57afe
abcdefg123 <383569403@qq.com>
2019-02-21 17:29:29 +0800

    删除注释

--- src/common/fastclick.js
+++ src/common/fastclick.js
@@ -3,7 +3,6 @@ Vue.directive('vtap', {
     var startTx, startTy, endTx, endTy, startTime, endTime;
 
     el.addEventListener("touchstart", function (e) {
-      el.classList.add("self-hover");
       var touch = e.touches[0];
       startTx = touch.clientX;
       startTy = touch.clientY;
@@ -11,10 +10,8 @@ Vue.directive('vtap', {
     }, false);
 
     el.addEventListener("touchend", function (e) {
-      el.classList.remove("self-hover");
       endTime = +new Date()
       if (endTime - startTime > 300) {
-        // 若点击事件过长，不执行回调
         return
       }
 
@@ -22,7 +19,6 @@ Vue.directive('vtap', {
       endTx = touch.clientX;
       endTy = touch.clientY;
       if (Math.abs(startTx - endTx) < 6 && Math.abs(startTy - endTy) < 6) {
-        // 若点击期间手机移动距离过长，不执行回调
         var method = binding.value.method;
         var params = binding.value.params;
         method(params);
